250f7459e42f0df4bc1444dea9dc43b6fc56b187
oh young seok <oyg9731@naver.com>
2023-09-02 14:31:46 +0900

    # 배열 알고리즘, 표준 입출력

---
+++ src/day01/ArrayDelete.java
@@ -0,0 +1,24 @@
+package day01;
+
+import java.util.Arrays;
+
+public class ArrayDelete {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int deleteTargetIndex = 2;
+
+        for (int i = deleteTargetIndex; i < arr.length - 1; i++)
+
+        arr[2] = arr[3];
+        arr[3] = arr[4];
+        arr[4] = arr[5];
+
+        int[] temp = new int[arr.length - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = arr[i];
+        }
+        arr = temp; temp = null;
+
+        System.out.println("arr = " + Arrays.toString(arr));
+    }
+}
